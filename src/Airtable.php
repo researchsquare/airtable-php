0ca7a55214572432beb8b0fb26e15026d806c8cd
Ian Welsh <iantylerwelsh@gmail.com>
2020-04-17 13:55:42 -0400

    new: add redis locking

--- src/Airtable.php
+++ src/Airtable.php
@@ -16,6 +16,8 @@ class Airtable
     private $key;
     private $base;
     private $requestOptions;
+    private $redis;
+    private $redisKey;
 
     public function __construct($config)
     {
@@ -23,6 +25,7 @@ class Airtable
             $this->setKey($config['api_key']);
             $this->setBase($config['base']);
             $this->setRequestOptions($config['request_options'] ?? []);
+            $this->setRedisLock($config['redisLock'] ?? []);
         } else {
             echo 'Error: __construct() - Configuration data is missing.';
         }
@@ -61,36 +64,67 @@ class Airtable
         $this->requestOptions = $requestOptions;
     }
 
+    public function setRedisLock(array $redisOptions)
+    {
+        if (
+            isset($redisOptions['client'])
+            && isset($redisOptions['key'])
+        ) {
+            $this->redis = $redisOptions['client'];
+            $this->redisKey = $redisOptions['key'];
+        }
+    }
+
     public function getContent($table, $data = [], $relations = false)
     {
-        return new Request($this, $table, $data, false, $relations, $this->requestOptions);
+        $this->acquireLock();
+
+        $res = new Request($this, $table, $data, false, $relations, $this->requestOptions);
+
+        $this->releaseLock();
+
+        return $res;
     }
 
     public function saveContent($table, $fields)
     {
+        $this->acquireLock();
+
         $fields = ['fields' => $fields];
         $request = new Request($this, $table, $fields, true, $this->requestOptions);
+
+        $this->releaseLock();
 
         return $request->getResponse();
     }
 
     public function updateContent($table, $fields)
     {
+        $this->acquireLock();
+
         $fields = ['fields' => $fields];
         $request = new Request($this, $table, $fields, 'patch', $this->requestOptions);
+
+        $this->releaseLock();
 
         return $request->getResponse();
     }
 
     public function deleteContent($table)
     {
+        $this->acquireLock();
+
         $request = new Request($this, $table, [], 'delete', $this->requestOptions);
+
+        $this->releaseLock();
 
         return $request->getResponse();
     }
 
     public function quickCheck($table, $field = '', $value = '')
     {
+        $this->acquireLock();
+
         $params = '';
         if (!empty($field)&& !empty($value)) {
             $params = array(
@@ -104,6 +138,37 @@ class Airtable
         $results['count'] = count($response->records);
         $results['records'] = $response->records;
 
+        $this->releaseLock();
+
         return (object)$results;
+    }
+
+    private function useRedisLock()
+    {
+        return !is_null($this->redis) && is_string($this->redisKey);
+    }
+
+    private function acquireLock()
+    {
+        if (!$this->useRedisLock()) {
+            return;
+        }
+
+        $lockAttempts = 0;
+        while ($this->redis->exists($this->redisKey) && $lockAttempts < 30) {
+            sleep(2);
+            $lockAttempts++;
+        }
+        // Acquire redis lock, expires in 60 seconds
+        $this->redis->set($this->redisKey, 1, 'EX', 60);
+    }
+
+    private function releaseLock()
+    {
+        if (!$this->useRedisLock()) {
+            return;
+        }
+
+        $this->redis->del([$this->redisKey]);
     }
 }
